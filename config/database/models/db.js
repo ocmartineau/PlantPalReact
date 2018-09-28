44221ef9254fa127ee631d0537c05d5b2ec62ea4
Olivier Martineau <omartineau8@gmail.com>
2018-09-27 21:26:16 -0400

    updating database config

--- config/database/models/db.js
+++ config/database/models/db.js
@@ -5,18 +5,18 @@ const UserPlantModel = require('../models/UserPlant');
 const SystemPlantModel = require('../models/SystemPlant');
 const ScheduleDayModel = require('../models/ScheduleDay');
 
-// const db = new Sequelize('plant_pal', 'root', 'calyps0', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     operatorsAliases: false,
-//
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     },
-// });
+const db = new Sequelize('plant_pal', 'root', 'calyps0', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
 
 
 const User = UserModel(db, Sequelize);
@@ -28,12 +28,12 @@ ScheduleDay.belongsToMany(UserPlant, {through: 'userPlantSchedule'});
 UserPlant.belongsToMany(ScheduleDay, {through: 'userPlantSchedule'});
 UserPlant.belongsTo(User);
 
-// db.sync({
-//     force: true
-//
-// }).then(() => {
-//     console.log("Database Created!")
-// });
+db.sync({
+    force: true
+
+}).then(() => {
+    console.log("Database Created!")
+});
 module.exports = {
     'User' : User,
     'UserPlant': UserPlant,
--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const env = require('dotenv').load();
 const mysql2 = require("mysql2");
-var Sequelize = require('sequelize');
-// const db = require('./config/database/models/db')
+const db = require('./config/database/models/db');
 // Requiring passport as we've configured it
 const passport = require("passport");
 // Setting up port and requiring models for syncing
@@ -22,44 +21,6 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Requiring our routes
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-
-//Models
-// const { User, UserPlant, SystemPlant, ScheduleDay } = require("./config/database/models");
-
-//Sync Database
-
-const db = new Sequelize('plant_pal', 'root', 'calyps0', {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorsAliases: false,
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-});
-
-db.sync({
-    force: true
-
-}).then(() => {
-    console.log("Database Created!")
-});
-// models.sequelize.sync().then(function() {
-//
-//     console.log('Nice! Database looks fine')
-//
-// }).catch(function(err) {
-//
-//     console.log(err, "Something went wrong with the Database Update!")
-//
-// });
 
 app.listen(PORT, function() {
     console.log(
